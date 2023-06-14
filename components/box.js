709123fa78c0d647c0f460a9d620f50a2331b5f6
Hurmis India <prescripson@gmail.com>
2023-06-14 16:08:48 +0530

    otpless intergration

---
+++ components/box.js
@@ -0,0 +1,10 @@
+export default function Box({className}) {
+  return (
+    <div
+     
+      className={"border-4 border-red-500 border-solid" + className}
+    >
+      hello this is text
+    </div>
+  );
+}
